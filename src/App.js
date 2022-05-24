cfe911edff81bbaf42d58543be9e6c4087697796
kevinpaulJose <bkevin1999@gmail.com>
2022-05-25 00:29:48 +0530

    Routing changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import RouterComponent from "./components/RoutingComponent";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
-  return <RouterComponent />;
+  return (
+    <BrowserRouter>
+      <RouterComponent />
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/components/RoutingComponent.js
+++ src/components/RoutingComponent.js
@@ -1,39 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import GeneralListComponent from "./GeneralListComponents/GeneralListComponent";
 
 export default function RouterComponent() {
   return (
-    <Router>
-      <div>
-        {/* <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav> */}
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Routes>
-          <Route path="/about" element={<About />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/" element={<GeneralListComponent />} />
-        </Routes>
-      </div>
-    </Router>
+    <Routes>
+      <Route path="/about" element={<About />} />
+      <Route path="/users" element={<Users />} />
+      <Route path="/" element={<GeneralListComponent />} />
+    </Routes>
   );
-}
-
-function Home() {
-  return <h2>Home</h2>;
 }
 
 function About() {
